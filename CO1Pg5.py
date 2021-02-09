d8cc92f3933874bebbd77c80bdef5e3da66975b2
abeelashraf98 <76091918+abeelashraf98@users.noreply.github.com>
2021-02-10 00:10:50 +0530

    Update CO1Pg5.py

--- CO1Pg5.py
+++ CO1Pg5.py
@@ -5,7 +5,7 @@ for i in range(0,s):
     n.append(int(input()))
 print("list values")
 print(n)
-print("The list after assinging:")
+print("The list after assigning:")
 for i in range(0,len(n)):
     if n[i]>=100:
         n[i]="over"
